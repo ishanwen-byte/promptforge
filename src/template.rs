7eaf15a664d1324c7e0872ad7c953088521a2a35
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-11 08:49:23 -0500

    add template fn requirement

--- src/template.rs
+++ src/template.rs
@@ -3,6 +3,7 @@ use std::collections::HashMap;
 
 pub trait Template {
     fn format(&self, variables: HashMap<&str, &str>) -> Result<String, TemplateError>;
+    fn template(&self) -> &str;
     fn template_format(&self) -> TemplateFormat;
     fn input_variables(&self) -> Vec<String>;
 }
