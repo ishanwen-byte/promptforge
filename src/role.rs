e2a43fd06cc196055bf50934a83b80506e42e0ef
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-11 21:22:41 -0500

    change to enum for easier use

--- src/role.rs
+++ src/role.rs
@@ -1,7 +1,37 @@
-pub struct Role(pub &str);
+use std::convert::TryFrom;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Role {
+    System,
+    Human,
+    Ai,
+    Tool,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct InvalidRoleError;
+
+impl TryFrom<&str> for Role {
+    type Error = InvalidRoleError;
+
+    fn try_from(role: &str) -> Result<Self, Self::Error> {
+        match role.to_lowercase().as_str() {
+            "system" => Ok(Role::System),
+            "human" => Ok(Role::Human),
+            "ai" => Ok(Role::Ai),
+            "tool" => Ok(Role::Tool),
+            _ => Err(InvalidRoleError),
+        }
+    }
+}
 
 impl Role {
-    pub const AI: Role = Role("ai");
-    pub const HUMAN: Role = Role("human");
-    pub const SYSTEM: Role = Role("system");
+    pub fn as_str(&self) -> &str {
+        match self {
+            Role::System => "system",
+            Role::Human => "human",
+            Role::Ai => "ai",
+            Role::Tool => "tool",
+        }
+    }
 }
