d6b6f0eefcf11daa079f95451de84220a6c527fa
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-13 08:51:38 -0500

    from base message accepts box

--- src/message_like.rs
+++ src/message_like.rs
@@ -1,34 +1,34 @@
 use crate::prompt_template::PromptTemplate;
+use crate::role::Role;
 use messageforge::BaseMessage;
 use std::sync::Arc;
 
 #[derive(Debug, Clone)]
 pub enum MessageLike {
     BaseMessage(Arc<dyn BaseMessage>),
-    PromptTemplate(Arc<PromptTemplate>),
+    RolePromptTemplate(Role, Arc<PromptTemplate>),
 }
 
 impl MessageLike {
-    pub fn from_base_message<T: BaseMessage + 'static>(message: T) -> Self {
-        MessageLike::BaseMessage(Arc::new(message))
+    pub fn from_base_message(message: Box<dyn BaseMessage>) -> Self {
+        MessageLike::BaseMessage(Arc::from(message))
     }
 
-    pub fn from_prompt_template(template: PromptTemplate) -> Self {
-        MessageLike::PromptTemplate(Arc::new(template))
+    pub fn from_role_prompt_template(role: &Role, template: PromptTemplate) -> Self {
+        MessageLike::RolePromptTemplate(*role, Arc::new(template))
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::Template;
-
     use super::*;
+    use crate::Template;
     use messageforge::MessageType;
     use messageforge::{AiMessage, HumanMessage, SystemMessage};
 
     #[test]
     fn test_from_base_message_human() {
-        let human_message = HumanMessage::new("Hello, how are you?");
+        let human_message = Box::new(HumanMessage::new("Hello, how are you?"));
 
         let message_like = MessageLike::from_base_message(human_message);
 
@@ -42,7 +42,7 @@ mod tests {
 
     #[test]
     fn test_from_base_message_ai() {
-        let ai_message = AiMessage::new("I am an AI.");
+        let ai_message = Box::new(AiMessage::new("I am an AI."));
 
         let message_like = MessageLike::from_base_message(ai_message);
 
@@ -56,7 +56,7 @@ mod tests {
 
     #[test]
     fn test_from_base_message_system() {
-        let system_message = SystemMessage::new("You are a helpful assistant.");
+        let system_message = Box::new(SystemMessage::new("You are a helpful assistant."));
 
         let message_like = MessageLike::from_base_message(system_message);
 
@@ -69,21 +69,22 @@ mod tests {
     }
 
     #[test]
-    fn test_from_prompt_template() {
+    fn test_from_role_prompt_template() {
         let template = PromptTemplate::new("Hello, {name}!").unwrap();
-        let message_like = MessageLike::from_prompt_template(template);
+        let message_like = MessageLike::from_role_prompt_template(&Role::Human, template);
 
-        if let MessageLike::PromptTemplate(tmpl) = message_like {
+        if let MessageLike::RolePromptTemplate(role, tmpl) = message_like {
+            assert_eq!(role, Role::Human);
             assert_eq!(tmpl.template(), "Hello, {name}!");
             assert_eq!(tmpl.input_variables(), vec!["name"]);
         } else {
-            panic!("Expected MessageLike::PromptTemplate variant.");
+            panic!("Expected MessageLike::RolePromptTemplate variant.");
         }
     }
 
     #[test]
     fn test_clone_message_like() {
-        let human_message = HumanMessage::new("Hello!");
+        let human_message = Box::new(HumanMessage::new("Hello!"));
         let message_like = MessageLike::from_base_message(human_message);
         let cloned_message_like = message_like.clone();
 
@@ -94,13 +95,14 @@ mod tests {
         }
 
         let template = PromptTemplate::new("Hello, {name}!").unwrap();
-        let message_like = MessageLike::from_prompt_template(template);
+        let message_like = MessageLike::from_role_prompt_template(&Role::Ai, template);
         let cloned_message_like = message_like.clone();
 
-        if let MessageLike::PromptTemplate(tmpl) = cloned_message_like {
+        if let MessageLike::RolePromptTemplate(role, tmpl) = cloned_message_like {
+            assert_eq!(role, Role::Ai);
             assert_eq!(tmpl.template(), "Hello, {name}!");
         } else {
-            panic!("Expected MessageLike::PromptTemplate variant.");
+            panic!("Expected MessageLike::RolePromptTemplate variant.");
         }
     }
 }
