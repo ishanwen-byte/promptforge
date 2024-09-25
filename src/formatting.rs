4a4848e50cb8b101548e08576024e37f383d40d7
Kingston Huynh <139024820+kinghuynh@users.noreply.github.com>
2024-09-25 03:39:47 -0500

    remove generics

--- src/formatting.rs
+++ src/formatting.rs
@@ -1,21 +1,11 @@
 use crate::template_format::{TemplateError, TemplateFormat};
 use std::collections::HashMap;
-use std::fmt::Display;
-use std::hash::Hash;
 
-pub trait Formattable<K, V>
-where
-    K: Into<String> + Hash + Eq,
-    V: Into<String> + Display,
-{
-    fn format(&self, variables: &HashMap<K, V>) -> Result<String, TemplateError>;
+pub trait Formattable {
+    fn format(&self, variables: &HashMap<&str, &str>) -> Result<String, TemplateError>;
 }
 
-pub trait Templatable<K, V>: Formattable<K, V>
-where
-    K: Into<String> + Hash + Eq,
-    V: Into<String> + Display,
-{
+pub trait Templatable: Formattable {
     fn template(&self) -> &str;
     fn template_format(&self) -> TemplateFormat;
     fn input_variables(&self) -> Vec<String>;
